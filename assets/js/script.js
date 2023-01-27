eee196cbdb12568238198e9eaf8e6e36cdf56e08
jshimetz <jeremyshimetz@gmail.com>
2023-01-26 18:45:39 -0600

    Adding working API code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,43 @@
+const api_url ="https://zenquotes.io/api/random/";
+const ul = document.querySelector('.quote-list');
 
-const inspQuotes = document.getElementById('inspQuotes');
+async function getapi(url)
+{
+  const response = await fetch(url);
+  var data = await response.json();
+  console.log(data[0].q);
 
-fetch('https://zenquotes.io//api/random')
-  .then(response => response.json())
-  .then(data => {
+  ul.innerHTML += `<li>${data[0].q}</li>`;
+}
+
+getapi(api_url);
+
+// const inspQuotes = document.getElementById('inspQuotes');
+
+// fetch('https://zenquotes.io/api/random', {
+//   method: 'GET',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Access-Control-Allow-Origin':'*'
+//   }
+// })
+//   .then(response => response.json())
+//   .then(data => {
     
-    const quoteText = document.createElement('p');
-    quoteText.innerText = data.quoteText;
+//     const quoteText = document.createElement('p');
+//     quoteText.innerText = data.quoteText;
 
     
-    const quoteAuthor = document.createElement('p');
-    quoteAuthor.innerText = data.quoteAuthor;
+//     const quoteAuthor = document.createElement('p');
+//     quoteAuthor.innerText = data.quoteAuthor;
 
     
-    inspQuotes.appendChild(quoteText);
-    inspQuotes.appendChild(quoteAuthor);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+//     inspQuotes.appendChild(quoteText);
+//     inspQuotes.appendChild(quoteAuthor);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
 
 var openModal = document.querySelector(".openModal");
 var closeModal = document.querySelector("#closeModal")
@@ -34,44 +53,6 @@ closeModal.addEventListener("click", function () {
 
 
 
-// $("document").ready(function () {
-
-//   var url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
-
-//   loadQuote();
-
-//   $("#newQuote").on("click", function () {
-//     $("#quote-block").hide();
-//     loadQuote();
-//   });
-// }
-
-//     function loadQuote() {
-//     var random = Math.floor(Math.random() * 10);
-//     $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function (result) {
-//       //response data are now in the result variable
-//       //$(".quote").css("display","none");
-//       //   $(".author").css("display","none");
-//       $(".quote").html(result.quoteText);
-
-//       $(".author").html(result.quoteAuthor);
-//     }
-// }
-
-
-
-
-
-// fetch("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", {
-//     method: 'getQuote',
-//     format: 'json',
-//     lang: 'en',
-// })
-// .then (function (response) {
-//     return response.json();
-// })
-
-
 /**
  * This is an example of a basic node.js script that performs
  * the Client Credentials oAuth2 flow to authenticate against
@@ -81,38 +62,38 @@ closeModal.addEventListener("click", function () {
  * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
  */
 
-// var request = require('request'); // "Request" library
+var request = require('request'); // "Request" library
 
-// var client_id = 'CLIENT_ID'; // Your client id
-// var client_secret = 'CLIENT_SECRET'; // Your secret
+var client_id = 'CLIENT_ID'; // Your client id
+var client_secret = 'CLIENT_SECRET'; // Your secret
 
-// // your application requests authorization
-// var authOptions = {
-//   url: 'https://accounts.spotify.com/api/token',
-//   headers: {
-//     'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-//   },
-//   form: {
-//     grant_type: 'client_credentials'
-//   },
-//   json: true
-// };
+// your application requests authorization
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
 
-// request.post(authOptions, function(error, response, body) {
-//   if (!error && response.statusCode === 200) {
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
 
-//     // use the access token to access the Spotify Web API
-//     var token = body.access_token;
-//     var options = {
-//       url: 'https://api.spotify.com/v1/users/jmperezperez',
-//       headers: {
-//         'Authorization': 'Bearer ' + token
-//       },
-//       json: true
-//     };
-//     request.get(options, function(error, response, body) {
-//       console.log(body);
-//     });
-//   }
-// });
+    // use the access token to access the Spotify Web API
+    var token = body.access_token;
+    var options = {
+      url: 'https://api.spotify.com/v1/users/jmperezperez',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      json: true
+    };
+    request.get(options, function(error, response, body) {
+      console.log(body);
+    });
+  }
+});
 
